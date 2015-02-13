f39b80e899daed986cd88696201e4deba897b9d0
Agarimo <Agarimo@192.168.1.39>
2015-02-13 14:53:11 +0100

    Inserción de la Clase Log en el paquete.

---
+++ src/util/Log.java
@@ -0,0 +1,81 @@
+package util;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Agárimo
+ */
+public class Log {
+
+    private File dir;
+    private final File archivo;
+    private final boolean general;
+
+    public Log() {
+        this.general = true;
+        creaFichero();
+        this.archivo = new File(dir, "log.log");
+        creaArchivo();
+    }
+
+    public Log(String nombre) {
+        this.general = false;
+        creaFichero();
+        this.archivo = new File(dir, nombre + Dates.imprimeFecha(Dates.curdate()) + ".log");
+        creaArchivo();
+    }
+
+    private void creaFichero() {
+        this.dir = new File("log");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+    }
+
+    private void creaArchivo() {
+        if (!this.archivo.exists()) {
+            try {
+                this.archivo.createNewFile();
+                if (general) {
+                    escribeMsg("--LOG DE EJECUCION--");
+                } else {
+                    escribeMsg("REGISTRO DE EVENTOS DE EJECUCIÓN -- " + Dates.imprimeFecha(Dates.curdate()));
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void escribeMsg(String msg) {
+        String hora = Dates.imprimeFechaCompleta(Dates.curdate());
+
+        try {
+            try (BufferedWriter out = new BufferedWriter(new FileWriter(archivo, true))) {
+                out.write(hora + "\t" + msg);
+                out.newLine();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void escribeError(String error, String msg) {
+        String hora = Dates.imprimeFechaCompleta(Dates.curdate());
+
+        try {
+            try (BufferedWriter out = new BufferedWriter(new FileWriter(archivo, true))) {
+                out.write(hora + "\t" + error + "\t" + msg);
+                out.newLine();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
