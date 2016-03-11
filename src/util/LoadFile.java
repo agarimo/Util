cfeb7040293b11d531e5a30d5c1d8e2a18f0a4bb
Alberto García Sánchez <agarimosoft@gmail.com>
2016-03-11 13:40:56 +0100

    WRK 201603011

---
+++ src/util/LoadFile.java
@@ -0,0 +1,50 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Agarimo
+ */
+public class LoadFile {
+
+    private final File file;
+    private final List<String> lineas;
+    private int count;
+    private final boolean isCorrecto;
+    
+    public LoadFile(File file){
+        this.file=file;
+        count=0;
+        isCorrecto=true;
+        lineas=new ArrayList<>();
+        loadLines();
+    }
+    
+    private void loadLines() {
+        try (FileReader fr = new FileReader(this.file); BufferedReader br = new BufferedReader(fr)) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                lineas.add(linea);
+                count++;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(LoadFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public int getCount(){
+        return this.count;
+    }
+    
+    public List<String> getLineas(){
+        return this.lineas;
+    }
+}
